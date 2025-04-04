7f5d79b87c6fdc0610bf38787bf1ed741c7d5c61
A042XD <AlexYJHP@Outlook.com>
2025-04-04 11:05:47 -0400

    Now able to save and restore tasks.

--- main.py
+++ main.py
@@ -1,67 +1,74 @@
+import os, json
 import tkinter as tk
 from tkinter import ttk
-
-"""
-# Setup
-root = tk.Tk()
-root.title("Habits Tracker")
-root.geometry("400x600")
-content = ttk.Frame(root)
-
-# Widgets
-widgets = {}
-widgets["tasks_list"] = ttk.Frame(content)
-widgets["tasks_list"].pack(expand=True, fill=tk.BOTH, padx=5, pady=5)
-name = tk.StringVar(root)
-widgets["name_entry"] = tk.Entry(content, textvariable=name)
-widgets["name_entry"].pack(expand=False, fill=tk.X, padx=5, pady=5)
-
-class task:
-    def __init__(self, name):
-        self.name = name
-        self.root = None
-        self.frame = None
-    def complete(self):
-        self.frame.pack_forget()
-        del self
-    def widget_card(self, root):
-        self.root = root
-        self.frame = ttk.Frame(root)
-        label = ttk.Label(self.frame, text=self.name).pack(expand=False, fill=tk.NONE, padx=5, pady=5, side=tk.LEFT)
-        button = ttk.Button(self.frame, text="Complete", command=self.complete).pack(expand=False, fill=tk.NONE, padx=5, pady=5, side=tk.RIGHT)
-        self.frame.pack(fill=tk.X, padx=5, pady=5)
-def add_task():
-    tmp = task(name.get())
-    tmp.widget_card(widgets["tasks_list"])
-    name.set("")
-
-widgets["add_button"] = ttk.Button(content, text="Add task", command=add_task)
-widgets["add_button"].pack(expand=False, fill=tk.X, padx=5, pady=5)
-content.pack(expand=True, fill=tk.BOTH)
-
-# Mainloop
-root.mainloop()
-"""
-
-import os
 
 class File:
     def __init__(self, path):
         self.path = path
-    def open_create(self):
-        pass 
+        self.content = []
+    def read(self):
+        if os.path.exists(self.path) == False:
+            self.content = [""]
+        else:
+            with open(self.path, 'r') as file:
+                for line in file:
+                    self.content.append(line.strip())
+                file.close()
+    def save(self):
+        with open(self.path, 'w') as file:
+            file.writelines(self.content)
+            file.close()
+class JsonFile(File):
+    def __init__(self, path):
+        super().__init__(path)
+        self.content_single = ""
+        self.content_json = []
+    def restruct(self):
+        self.content_single = ""
+        for i in self.content:
+            self.content_single += i
+    def read(self):
+        File.read(self)
+        if self.content == [""] or self.content == []:
+            self.content_json = []
+        else:
+            self.restruct()
+            try:
+                self.content_json = json.loads(self.content_single)
+            except json.decoder.JSONDecodeError:
+                self.content_json = []
+    def save(self):
+        self.content = [json.dumps(self.content_json)]
+        File.save(self)
 class Task: 
-    def __init__(self, name, date):
+    def __init__(self, name):
         self.name = name
         self.start_date = ""
         self.completed = False
-class TasksPage:
-    def __init__(self, root):
+    def json(self):
+        obj = {"name": self.name}
+        return obj
+class TaskCard:
+    def __init__(self, root, task):
+        self.task = task
         self.root = root
+    def add_content(self):
+        self.content = ttk.Frame(self.root)
+        self.label = ttk.Label(self.content, text=self.task.name)
+        self.complete_button = ttk.Button(self.content, text="Complete")
+    def pack_content(self):
+        self.label.pack(expand=False, fill=tk.NONE, side=tk.LEFT)
+        self.complete_button.pack(expand=False, fill=tk.NONE, side=tk.RIGHT)
+        self.content.pack(expand=False, fill=tk.X, ipadx=5, ipady=5, padx=5)
+class TasksPage:
+    def __init__(self, root, tasks_file):
+        self.root = root
+        self.tasks_file = tasks_file
         self.content = ttk.Frame(self.root)
         self.content.pack(expand=True, fill=tk.BOTH)
         self.add_content()
         self.pack_content()
+        self.add_tasks_from_tasks_file()
     def add_content(self):
         self.caption = ttk.Label(self.content, text="Tasks")
         self.tasks_list = ttk.Frame(self.content)
@@ -74,23 +81,44 @@ class TasksPage:
         self.add_entry.pack(fill=tk.X, padx=5, pady=5)
         self.add_button.pack(fill=tk.X, padx=5, pady=5)
     def create_task(self):
+        task = Task(self.add_entry_var.get())
+        self.tasks_file.content_json.insert(0, task.json())
+        # print(self.tasks_file.content_json)
+        tmp = TaskCard(self.tasks_list, task)
+        tmp.add_content()
+        tmp.pack_content()
         self.add_entry_var.set("")
-class App:
-    def __init__(self, root):
+    def add_tasks_from_tasks_file(self):
+        for i in self.tasks_file.content_json:
+            tmp = TaskCard(self.tasks_list, Task(i["name"]))
+            tmp.add_content()
+            tmp.pack_content()
+class AppInterface:
+    def __init__(self, root, tasks_file):
         self.root = root
         self.notebook = ttk.Notebook(self.root)
         self.content1 = ttk.Frame(self.root)
         self.content2 = ttk.Frame(self.root)
         self.content3 = ttk.Frame(self.root)
-        TasksPage(self.content1)
+        TasksPage(self.content1, tasks_file)
         self.notebook.add(self.content1, text="Tasks")
         self.notebook.add(self.content2, text="Habits")
         self.notebook.add(self.content3, text="Statistics")
         self.notebook.pack(expand=True, fill=tk.BOTH)
+class App:
+    def __init__(self):
+        self.tasks_path = os.getcwd() + "/tasks.json"
+        self.tasks_file = JsonFile(self.tasks_path)
+        self.tasks_file.read()
+        
+        # App Interface
+        self.root = tk.Tk()
+        self.root.title("Todo App")
+        self.root.geometry("400x600+200+100")
+        self.root.resizable(False, False)
+        AppInterface(self.root, self.tasks_file)
+        self.root.mainloop()
+
+        self.tasks_file.save()
 if __name__ == "__main__":
-    root = tk.Tk()
-    root.title("Todo App")
-    root.geometry("400x600+200+100")
-    root.resizable(False, True)
-    App(root)
-    root.mainloop()
+    App()
