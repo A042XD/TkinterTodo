88f6b2eca6bb7804ab0092cdc4b50018b2a9cdfb
A042XD <AlexYJHP@Outlook.com>
2025-04-03 23:21:38 -0400

    Added tasks list

--- main.py
+++ main.py
@@ -2,26 +2,39 @@ import tkinter as tk
 from tkinter import ttk
 
 root = tk.Tk()
-root.title("Todo-app")
-root.geometry("400x400")
+root.title("Habits Tracker")
+root.geometry("400x600")
 content = ttk.Frame(root)
 
-button = [ttk.Button(content, text="hello", command=None) for i in range(0,10)]
-for i in button:
-    i.pack()
-label = ttk.Label(content, text="test-text", foreground="white", width=50,background="black")
-label.pack()
+widgets = {}
+widgets["tasks_list"] = ttk.Frame(content)
+widgets["tasks_list"].pack(expand=True, fill=tk.BOTH, padx=5, pady=5)
+name = tk.StringVar(root)
+widgets["name_entry"] = tk.Entry(content, textvariable=name)
+widgets["name_entry"].pack(expand=False, fill=tk.X, padx=5, pady=5)
 
-entry = ttk.Entry(content)
-entry.insert(0,"卧槽")
-entry.delete(0,tk.END)
-entry.get()
+class task:
+    def __init__(self, name):
+        self.name = name
+        self.root = None
+        self.frame = None
+    def complete(self):
+        self.frame.pack_forget()
+        del self
+    def widget_card(self, root):
+        self.root = root
+        self.frame = ttk.Frame(root)
+        label = ttk.Label(self.frame, text=self.name).pack(expand=False, fill=tk.NONE, padx=5, pady=5, side=tk.LEFT)
+        button = ttk.Button(self.frame, text="Complete", command=self.complete).pack(expand=False, fill=tk.NONE, padx=5, pady=5, side=tk.RIGHT)
+        self.frame.pack(fill=tk.X, padx=5, pady=5)
+def add_habit():
+    tmp = task(name.get())
+    tmp.widget_card(widgets["tasks_list"])
+    name.set("")
 
-text_box = tk.Text(content)
-text_box.get("1.0","2.5") # 从 (1,0) 到 (2,5) 的矩阵的内容
+widgets["add_button"] = ttk.Button(content, text="Add task", command=add_habit)
+widgets["add_button"].pack(expand=False, fill=tk.X, padx=5, pady=5)
 
-text_box.pack()
-entry.pack()
-
-content.pack()
+content.pack(expand=True, fill=tk.BOTH)
 root.mainloop()
+
